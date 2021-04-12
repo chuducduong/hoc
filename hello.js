c83ab0f1afa4144bea4fbd7be40f5df60494bbb6
Chu Duc Duong <ducduongk13d@gmail.com>
2021-04-12 16:16:18 +0700

    javascript

---
+++ hello.js
@@ -0,0 +1,11 @@
+let my_name = prompt("ban ten gì"); 
+
+alert("xinchao"  +  my_name);
+
+//chú thích 1 dòng 
+/*
+
+chú thích nhiều dòng
+
+
+*/
---
+++ hello2.js
@@ -0,0 +1,8 @@
+function show_my_name() {
+    let my_name = prompt("ban ten gì"); 
+
+   document.getElementById("name_placeholder").innerHTML = "xin chao" + my_name;
+}
+
+/*đây là hàm tự định nghĩa ra 
+  */
