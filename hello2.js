0e5075a5345dc863b701fe26f64195c2278c36c3
Chu Duc Duong <ducduongk13d@gmail.com>
2021-04-12 17:24:55 +0700

    lol

--- hello2.js
+++ hello2.js
@@ -1,8 +1,17 @@
+
+
+      /*
 function show_my_name() {
     let my_name = prompt("ban ten gì"); 
 
    document.getElementById("name_placeholder").innerHTML = "xin chao" + my_name;
 }
 
-/*đây là hàm tự định nghĩa ra 
-  */
+//đây là hàm tự định nghĩa ra , ở đây khi nhập tên vào ô và nhấn buttton thì kết quả sẽ hiện lên 
+dòng chữ ở thẻ p
+      */
+
+function show_my_name() {
+    let my_name = prompt("ban ten gì"); 
+    document.write("xin chao" + my_name); /* những thẻ nào cũng cấp bên trong body thì nó xóa đi */
+}
