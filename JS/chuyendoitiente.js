309ed6b0c8e9a0428e1cabb0d733cb4fd905bb88
Chu Duc Duong <ducduongk13d@gmail.com>
2021-04-22 14:04:26 +0700

    lol

---
+++ JS/chuyendoitiente.js
@@ -0,0 +1,18 @@
+function convert() {
+    // code
+
+    // lay gia tri tu o input
+    let amount = document.getElementById('amount').value;
+
+    //lay gia tri tu o select thong option duoc chon
+    let from = document.getElementById('from').value;
+    let to = document.getElementById('to').value;
+
+    //tinh toan
+    result = amount * to / from;
+
+    //Thay đổi nội dung thẻ h2
+    document.getElementById('result').innerHTML = result;
+
+}
+© 2021 GitHub, Inc.
